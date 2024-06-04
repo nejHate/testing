125257a9fab883aed74d656145708b7932cc6545
nejHate <63677856+nejHate@users.noreply.github.com>
2024-06-05 01:18:05 +0200

    Update main.cpp

--- opengl_testing/opengl_06/main.cpp
+++ opengl_testing/opengl_06/main.cpp
@@ -3,6 +3,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// if the window be resized, the glfwSetFrambufferSizeCallback will call this function
+// this function just change the glViewport
+void framebuffer_size_callback(GLFWWindow *window, int width, int height){
+  glViewport(0, 0, width, height);
+}
+
 int main()
 {
   // initialization of GLFW
@@ -21,6 +27,10 @@ int main()
   // first two arguments is width and height of the window
   // third argument is name of the window
   // last two arguments we can ignore (the tutorial said it later I will write what they mean) TODO
+  // by chatgpt:
+  // GLFWmonitor *monitor -> the monitor to use for full-screen mode (can be NULL for windowed mode)
+  // GLFWwindow *share -> The window whose context to share resources with (can be NULL)
+  // this command create a window of the size first and second parameter
   GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
   // check if the object was created
   // if no, print error and exit
@@ -43,8 +53,23 @@ int main()
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
-  //
+  
+  // I have some toughts what this do so sorry if the explanation isn't correct
+  // it will transform coordinates from (-1, 1) to (0, widows_size)
+  // so if we set window size and viewport to same number
+  // the coordination (-0.5, 0.5) to (200, 450)
+  // chatgpt: this defines the tranformation between normalized device coordinates and window coordinates
+  // if the numbers is larger, it will show only a portion of the image
+  // if the numbers is smaller, it will show the image in portion of the window surrounded by blank space
   glViewport(0, 0, 800, 600);
+  
+  // i am going by the book so now they create a function called framebuffer_size_callback
+  // look above to find it, it will be used in next code
+  
+  // there is many types of callback
+  // callback is registered after we create window and before the render loop
+  // this will tell the GLFW that to call function on every resize window event
+  glfwSetFrambufferSizeCallback(window, framebuffer_size_callback);
   
   return 0;
 }
