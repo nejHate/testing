8071b4337841078e2890b21c2f4fd136cb44b864
nejHate <63677856+nejHate@users.noreply.github.com>
2024-06-04 13:28:34 +0200

    Update main.cpp

--- opengl_testing/opengl_06/main.cpp
+++ opengl_testing/opengl_06/main.cpp
@@ -5,11 +5,16 @@
 
 int main()
 {
+  // initialization of GLFW
   glfwInit();
+  // setting major version
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  // setting minor version
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  // seting GLFW to core_profile
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  // for MAC (to run the app on mac)
+  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for MAC
   std::cout << "END OF GAME" << std::endl;
   return 0;
 }
