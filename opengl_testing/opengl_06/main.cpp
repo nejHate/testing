3ea44be19e22c2ee577dedcb691fbe0e03ed44f6
nejHate <63677856+nejHate@users.noreply.github.com>
2024-06-05 00:29:29 +0200

    Update main.cpp

--- opengl_testing/opengl_06/main.cpp
+++ opengl_testing/opengl_06/main.cpp
@@ -14,7 +14,37 @@ int main()
   // seting GLFW to core_profile
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   // for MAC (to run the app on mac)
-  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for MAC
+  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for MAC
   std::cout << "END OF GAME" << std::endl;
+  
+  // object that holds window data
+  // first two arguments is width and height of the window
+  // third argument is name of the window
+  // last two arguments we can ignore (the tutorial said it later I will write what they mean) TODO
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  // check if the object was created
+  // if no, print error and exit
+  if(window == NULL){
+    std::cout << "Failed to create GLFW window " << __LINE__ << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  // if the window is created, we say that the window (object) is current
+  // by internet opengl is state machine and if we want to swith between more we will use this command
+  // (used in multithreaded opengl)
+  // (if I understand this correctly)
+  glfwMakeContextCurrent(window);
+  
+  // glad manages pointers to the opengl functions
+  // the function pointers is OS specific
+  // it will help us find correct function to the OS which the code is compiled for
+  // if it failed load, it return error and 
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  //
+  glViewport(0, 0, 800, 600);
+  
   return 0;
 }
