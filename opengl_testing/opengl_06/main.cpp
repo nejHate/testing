5b2086c01f8e161fed2e20d76bc781da256917d2
nejHate <63677856+nejHate@users.noreply.github.com>
2024-06-12 00:15:33 +0200

    Update main.cpp

--- opengl_testing/opengl_06/main.cpp
+++ opengl_testing/opengl_06/main.cpp
@@ -19,7 +19,7 @@ const char *vertexShaderSource = "#version 330 core\n"
 
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"void main()\"
+"void main()\n"
 "{\n"
 "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
