5e72d53da78fc8e1d396bbb837db9dd1ecd0ea5c
nejhate <jakub.steb@gmail.com>
2024-06-23 23:52:21 +0200

    add

--- region_by_distance/rbg_01/rbg.cpp
+++ region_by_distance/rbg_01/rbg.cpp
@@ -3,13 +3,13 @@
 #include <random>
 
 auto get_random_number(int max){
-  static std::random_device rd;
+	static std::random_device rd;
 	static std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
 	std::uniform_int_distribution<> dis(0, max);
 	return dis(gen);
 }
 
 int main(){
-  return 0;
-  
+	std::cout << 
+	return 0;
 }
